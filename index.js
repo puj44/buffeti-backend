37f94a112d952ecb1a5fcfc3de2dd33c2ef7b530
puj44 <pujan007mm@gmail.com>
2024-04-29 23:36:05 +0530

    fixes

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ require('dotenv').config()
 const app = express();
 const bodyParser = require('body-parser');
 const cors=require('cors');
-const corsOptions = {
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-    ],
-    credentials: true
-  }
+  app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:false}));
-app.use(cors(corsOptions));
+app.use(cors());
 app.set("trust proxy", true);
 
 app.get('/api', (req, res) => {
