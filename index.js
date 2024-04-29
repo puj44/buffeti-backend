14d2646f4442927b6c219c9e0a4294900bec7564
puj44 <pujan007mm@gmail.com>
2024-04-29 21:32:43 +0530

    test api

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 require('dotenv').config()
 const app = express();
+const bodyParser = require('body-parser');
+const cors=require('cors');
+const corsOptions = {
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+    ],
+    credentials: true
+  }
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors(corsOptions));
+app.set("trust proxy", true);
 
 app.get('/', (req, res) => {
-    res.send('Successful response.');
+    return res.status(200).send('Working');
 });
 
-app.listen(process.env.SERVER_PORT || 3001, () => console.log("Server started."));
+app.listen(process.env.PORT || 3001, process.env.HOST || "::");
