7b756ddca478869c46bdcbd7a10aba80dd191b53
puj44 <pujan007mm@gmail.com>
2024-06-19 13:47:11 +0530

    fix

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -4,10 +4,9 @@ const validator = require("../middlewares/validator/validator");
 const { getItems, getCategories} = require('../controllers/itemController');
 const { itemRequests } = require('../middlewares/requests/itemRequests');
 const slugs = require('../middlewares/validator/slugs');
-const validateLocation = require('../middlewares/validateLocation');
 
 
-router.get(`/get-categories/:menuOption(${slugs.menuOption})` ,getCategories);
-router.get(`/get-items/:menuOption(${slugs.menuOption})/:category` ,validator(itemRequests.getItems, true) ,getItems);
+router.get(`/get-categories/:menuOption(${slugs.itemsMenuOption})` ,getCategories);
+router.get(`/get-items/:menuOption(${slugs.itemsMenuOption})/:category` ,validator(itemRequests.getItems, true) ,getItems);
 
 module.exports = router;
