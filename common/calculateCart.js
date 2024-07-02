fc84e40ed1effcb6d20e5264d9fb5bba7b9b4ad4
drachit631 <drachit631@gmail.com>
2024-07-02 10:09:42 +0530

    calculatePricing Click2cater

--- common/calculateCart.js
+++ common/calculateCart.js
@@ -1,6 +1,7 @@
 const { get } = require("./redisGetterSetter");
-const keys = require("../config/keys");
+const { Cart, CartItems } = require("../db/models/cart");
 
+//Validate Package, in case of click2cater
 async function validatePackage(items, isValidPackage, packages) {
   let values,
     categoriesMapings = packages.categories_mapping;
@@ -19,6 +20,7 @@ async function validatePackage(items, isValidPackage, packages) {
   return isValidPackage;
 }
 
+//Calculates the pricing of Cart items
 async function calculateItems(data, itemsData) {
   let additional_qty = null,
     extra_items = null,
@@ -26,6 +28,7 @@ async function calculateItems(data, itemsData) {
     total_price = 0,
     addon_charges = 0,
     items_id,
+    items_name,
     itemObj = {};
 
   console.log(data.menu_option);
@@ -55,6 +58,7 @@ async function calculateItems(data, itemsData) {
               if (itemsData[i].slug === key) {
                 if (additional_qty) {
                   items_id = itemsData[i]._id;
+                  items_name = itemsData[i].name;
                   addon_charges =
                     addon_charges +
                     itemsData[i].additional_serving_rate * additional_qty;
@@ -96,7 +100,6 @@ async function calculateItems(data, itemsData) {
       case "snack-boxes":
         data.items.forEach((values, keys) => {
           values.forEach(async (value, key) => {
-            console.log(key);
             Object.keys(itemsData).forEach((i) => {
               if (itemsData[i].slug === key) {
                 items_id = itemsData[i]._id;
@@ -107,6 +110,7 @@ async function calculateItems(data, itemsData) {
             });
             itemObj[key] = {
               item_id: items_id,
+              item_name: items_name,
               additional_qty: additional_qty,
               added_extra_items: extra_items,
               selected_preparation: preparation,
@@ -145,11 +149,230 @@ async function calculateItems(data, itemsData) {
       message: "Couldn't find data",
     });
   }
+}
 
-  return itemObj;
+//Calculates the pricing of whole Cart
+async function calculatePricing(id) {
+  let cart, cartItems;
+
+  cart = await Cart.findOne({ customer_id: id }).then((d) => d);
+
+  if (!cart) {
+    return sendError(res, "Oops! There is nothing here!", 404);
+  }
+
+  cartItems = await CartItems.findOne({ cart_id: cart?._id }).then((d) => d);
+
+  if (!cartItems) {
+    return sendError(res, "Oops! There is nothing here!", 404);
+  }
+
+  const {
+    menu_option,
+    location,
+    extra_services,
+    delivery_address_id,
+    delivery_date,
+    delivery_time,
+    cooking_instruction,
+  } = cart;
+
+  const { no_of_people, package_name, items } = cartItems;
+
+  let packagesData,
+    itemsData,
+    miniMealsData,
+    isValidPackage = true,
+    total_items_amount = 0, //for click2cater
+    total_amount = 0,
+    gst = 5,
+    total_billed_amount = 0,
+    packagePrice = 0,
+    itemObj;
+
+  const data = {
+    items: items,
+    menu_option: menu_option,
+    no_of_people: no_of_people,
+    isValidPackage: isValidPackage,
+    location: location,
+  };
+
+  const globalObj = {};
+
+  switch (menu_option) {
+    case "click2cater":
+      if (package_name) {
+        packagesData = await Packages.findOne({ slug: package_name }).then(
+          (d) => d
+        );
+        isValidPackage = await validatePackage(
+          items,
+          isValidPackage,
+          packagesData
+        );
+      }
+      itemsData = await findItems(items, menu_option);
+
+      //calculation of total amount
+      if (itemsData && packagesData) {
+        itemObj = await calculateItems(data);
+
+        if (no_of_people >= 10 && no_of_people <= 20) {
+          packagePrice = packagesData._10_20_pax;
+        } else if (no_of_people >= 20 && no_of_people <= 30) {
+          packagePrice = packagesData._20_30_pax;
+        } else {
+          packagePrice = packagesData._30_plus_pax;
+        }
+
+        Object.keys(itemObj).forEach((i) => {
+          total_items_amount = total_items_amount + itemObj[i].total_price;
+          globalObj[billing_details][item_pricing][amount] = total_items_amount;
+
+          globalObj[billing_details][item_pricing][qty] +=
+            itemObj[i].additional_qty + no_of_people;
+
+          globalObj[billing_details][item_pricing][item_name] =
+            itemObj[i].item_name;
+        });
+      } else {
+        return sendRes(res, 500, {
+          message: "Couldn't find data",
+        });
+      }
+      if (isValidPackage) {
+        total_amount += total_items_amount + packagePrice;
+      }
+
+      total_billed_amount += total_amount + (total_amount * gst) / 100;
+
+      globalObj = {
+        cart_id: cart?._id,
+        menu_option: menu_option,
+        delivery_address_id: delivery_address_id,
+        cart_data: {
+          cart_item_id: cartItems?._id,
+          package_name: package_name,
+          no_of_people: no_of_people,
+          items: itemObj,
+        },
+        delivery_date: delivery_date,
+        delivery_time: delivery_time,
+        cooking_instruction: cooking_instruction,
+        extra_services: extra_services,
+        coupon_code: coupon_code,
+        billing_details: {
+          item_pricing: [
+            {
+              // "item_name":"",
+              // "qty":"",
+              // "amount":"[amount]"
+            },
+          ],
+          total_amount: total_amount,
+          total_billed_amount: total_billed_amount,
+        },
+      };
+
+      break;
+    case "snack-boxes":
+      itemsData = await findItems(items, menu_option, package_name);
+      if (itemsData) {
+        itemObj = await calculateItems(data, itemsData);
+        Object.keys(itemObj).forEach((i) => {
+          total_amount += itemObj[i].total_price;
+        });
+      }
+
+      globalObj = {
+        cart_id: cart?._id,
+        menu_option: menu_option,
+        delivery_address_id: delivery_address_id,
+        cart_data: {
+          cart_item_id: cartItems?._id,
+          package_name: package_name,
+          no_of_people: no_of_people,
+          items: itemObj,
+        },
+        delivery_date: delivery_date,
+        delivery_time: delivery_time,
+        cooking_instruction: cooking_instruction,
+        extra_services: extra_services,
+        coupon_code: coupon_code,
+        billing_details: {
+          item_pricing: [
+            // {
+            //     "item_name":"",
+            //     "qty":"",
+            //     "amount":"[amount]"
+            // }
+          ],
+          total_amount: total_amount,
+          total_billed_amount: total_billed_amount,
+        },
+      };
+
+      break;
+    case "mini-meals":
+      itemsData = await findItems(items, menu_option, package_name);
+      if (itemsData) {
+        itemObj = await calculateItems(data, itemsData);
+        Object.keys(itemObj).forEach((i) => {
+          total_amount += itemObj[i].total_price;
+        });
+      }
+      globalObj = {
+        cart_id: cart?._id,
+        menu_option: menu_option,
+        delivery_address_id: delivery_address_id,
+        cart_data: {
+          items: {
+            // "slug":{
+            //     "cart_item_id",
+            //     "qty",
+            //     "package_name",
+            //      "package_price"
+            // },
+            // "slug2":{
+            //     "cart_item_id",
+            //     "qty",
+            //     "package_name",
+            //     "package_price"
+            // }
+          },
+        },
+        delivery_date: delivery_date,
+        delivery_time: delivery_time,
+        cooking_instruction: cooking_instruction,
+        extra_services: extra_services,
+        coupon_code: coupon_code,
+        billing_details: {
+          item_pricing: [
+            // {
+            //     "item_name":"",
+            //     "qty":"",
+            //     "amount":"[amount]"
+            // }
+          ],
+          total_amount: total_amount,
+          total_billed_amount: total_billed_amount,
+        },
+      };
+
+      break;
+  }
+
+  await Cart.deleteMany();
+  await CartItems.deleteMany();
+
+  return sendRes(res, 200, {
+    globalObj,
+  });
 }
 
 module.exports = {
   validatePackage,
   calculateItems,
+  calculatePricing,
 };
--- controllers/cartController.js
+++ controllers/cartController.js
@@ -7,6 +7,7 @@ const sendError = require("../common/sendError");
 const sendRes = require("../common/sendResponse");
 const { findItems } = require("../common/findItems");
 
+//Add To Cart
 const addtocart = async (req, res) => {
   const { id } = req.user ?? {};
   const { location } = req.headers;
@@ -26,6 +27,7 @@ const addtocart = async (req, res) => {
     });
 
     const cart = await Cart.findOne({ customer_id: id }).then((d) => d);
+    const cart_id = cart?._id;
 
     if (cart) {
       if (cart.menu_option !== menu_option) {
@@ -33,43 +35,42 @@ const addtocart = async (req, res) => {
           is_invalid: true,
         });
       }
-      return sendRes(res, 400, {
-        message: "Cart already exists",
-      });
-    } else {
-      cartInsert = await Cart.create({
+    }
+
+    cartInsert =
+      cart_id ??
+      (await Cart.create({
         customer_id: id,
         menu_option: menu_option,
         location: location,
         extra_services: extra_services,
       })
         .then((data) => data)
+        .catch((err) => err));
+
+    if (cartInsert) {
+      cartItemsInsert = await CartItems.create({
+        cart_id: cartInsert._id,
+        no_of_people: no_of_people,
+        package_name: package_name,
+        items: items,
+      })
+        .then((data) => data)
         .catch((err) => err);
 
-      if (cartInsert) {
-        cartItemsInsert = await CartItems.create({
-          cart_id: cartInsert._id,
-          no_of_people: no_of_people,
-          package_name: package_name,
-          items: items,
-        })
-          .then((data) => data)
-          .catch((err) => err);
-
-        if (cartItemsInsert) {
-          return sendRes(res, 200, {
-            message: "Cart item inserted successfully",
-          });
-        } else {
-          sendRes(res, 500, {
-            message: "Couldn't inserted cart item",
-          });
-        }
+      if (cartItemsInsert) {
+        return sendRes(res, 200, {
+          message: "Cart item inserted successfully",
+        });
       } else {
         sendRes(res, 500, {
-          message: "Couldn't inserted cart",
+          message: "Couldn't inserted cart item",
         });
       }
+    } else {
+      sendRes(res, 500, {
+        message: "Couldn't inserted cart",
+      });
     }
   } catch (err) {
     sendError(res, err);
@@ -80,99 +81,8 @@ const addtocart = async (req, res) => {
 const getCart = async (req, res) => {
   try {
     const { id } = req.user ?? {};
-    let cart, cartItems;
 
-    cart = await Cart.findOne({ customer_id: id }).then((d) => d);
-
-    if (!cart) {
-      return sendError(res, "Oops! There is nothing here!", 404);
-    }
-
-    cartItems = await CartItems.findOne({ cart_id: cart?._id }).then((d) => d);
-
-    if (!cartItems) {
-      return sendError(res, "Oops! There is nothing here!", 404);
-    }
-
-    const { menu_option, location, extra_services } = cart;
-    const { no_of_people, package_name, items } = cartItems;
-
-    let packagesData,
-      itemsData,
-      miniMealsData,
-      isValidPackage = true,
-      total_items_amount,
-      total_amount,
-      packagePrice,
-      itemObj;
-
-    const data = {
-      items: items,
-      menu_option: menu_option,
-      no_of_people: no_of_people,
-      isValidPackage: isValidPackage,
-      location: location,
-    };
-
-    switch (menu_option) {
-      case "click2cater":
-        if (package_name) {
-          packagesData = await Packages.findOne({ slug: package_name }).then(
-            (d) => d
-          );
-          isValidPackage = await validatePackage(
-            items,
-            isValidPackage,
-            packagesData
-          );
-        }
-        itemsData = await findItems(items, menu_option);
-
-        //calculation of total amount
-        if (itemsData && packagesData) {
-          itemObj = await calculateItems(data);
-
-          if (no_of_people >= 10 && no_of_people <= 20) {
-            packagePrice = packagesData._10_20_pax;
-          } else if (no_of_people >= 20 && no_of_people <= 30) {
-            packagePrice = packagesData._20_30_pax;
-          } else {
-            packagePrice = packagesData._30_plus_pax;
-          }
-
-          Object.keys(itemObj).forEach((i) => {
-            total_items_amount = total_items_amount + itemObj[i].total_price;
-          });
-        } else {
-          return sendRes(res, 500, {
-            message: "Couldn't find data",
-          });
-        }
-        if (isValidPackage) {
-          total_amount += total_items_amount + packagePrice;
-        }
-
-        break;
-      case "snack-boxes":
-        itemsData = await findItems(items, menu_option, package_name);
-        if (itemsData) {
-          itemObj = await calculateItems(data, itemsData);
-          console.log(itemObj);
-        }
-
-        break;
-      case "mini-meals":
-        itemsData = await findItems(items, menu_option, package_name);
-        if (itemsData) {
-          itemObj = await calculateItems(data, itemsData);
-          console.log(itemObj);
-        }
-
-        break;
-    }
-    return sendRes(res, 200, {
-      itemObj,
-    });
+    const cartObject = calculatePricing(id);
   } catch (err) {
     sendError(res, err);
   }
