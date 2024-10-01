0cfec34fa5c37bc575b1d872cc90e75e193294d0
Pujan Mistry <74445835+puj44@users.noreply.github.com>
2024-10-01 10:54:29 +0530

    Update customerRequests.js

--- middlewares/requests/customerRequests.js
+++ middlewares/requests/customerRequests.js
@@ -4,7 +4,7 @@ const customerRequests = {
     name: Joi.string().required().label("Name"),
     mobile_number: Joi.numberString().len(10).required(),
     email: Joi.string().empty(["", null]),
-    // token:Joi.string().required().label("Token")
+    token:Joi.string().label("Token")
   },
 };
 
