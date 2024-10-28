e5f99f8e5da7229c948a8dad84ee60c7b9920b04
drach <drachit631@gmail.com>
2024-10-29 00:45:12 +0530

    check status done

--- routes/adminRoutes/orderRoutes.js
+++ routes/adminRoutes/orderRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { orderTransactionInfo } = require("../../admin/controllers/ordersController");
+const {
+  orderTransactionInfo,
+  getOrder,
+  getOrderInfo,
+} = require("../../admin/controllers/ordersController");
 const router = express.Router();
 
 router.post("/order-transaction-info", orderTransactionInfo);
+router.get("/get-order", getOrder);
+router.get("/get-order-info/:id", getOrderInfo);
 
 module.exports = router;
