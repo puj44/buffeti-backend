02c7e8197dec404bda964266e2a660dc3404fee8
drach <drachit631@gmail.com>
2024-11-11 10:30:01 +0530

    get orders and get customers done

--- admin/controllers/customerController.js
+++ admin/controllers/customerController.js
@@ -1,5 +1,32 @@
-const getCustomer = async (req, res) => {};
+const sendResponse = require("../../common/sendResponse");
+const { Customer } = require("../../db/models/customers");
+
+const getCustomers = async (req, res) => {
+  const { search, sort } = req.query;
+  const pipeline = {};
+  let sortOption = {};
+  try {
+    if (search) {
+      const [searchQuery, searchField] = search.split(",");
+
+      query[searchField] = { $regex: searchQuery, $options: "i" };
+    }
+    if (sort) {
+      const [sortField, sortOrder] = sort.split(",");
+      sortOption[sortField] = sortOrder === "a" ? 1 : -1;
+    }
+    const allCustomers = await Customer.find(query).sort(sortOption).lean();
+
+    if (!allCustomers.length) {
+      return sendResponse(res, 404, { message: "No customers found" });
+    }
+    return sendResponse(res, 200, { customers: allCustomers });
+  } catch (err) {
+    console.log("Get Customers Err:", err);
+    return sendResponse(res, 400, { message: err?.message });
+  }
+};
 
 const getCustomerDetails = async (req, res) => {};
 
-module.exports = { getCustomer, getCustomerDetails };
+module.exports = { getCustomers, getCustomerDetails };
