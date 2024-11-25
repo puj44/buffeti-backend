5ec41b58077784db3abc72f3e9d4d4c29c43546a
drach <drachit631@gmail.com>
2024-11-25 09:11:07 +0530

    indices check

--- db/models/packages.js
+++ db/models/packages.js
@@ -10,9 +10,7 @@ const packagesSchema = new Schema({
   categories_mapping: { type: Object, required: true },
   items_mapping: { type: [String], required: true },
   category: { type: Object, required: true },
-  _10_20_pax: { type: Number, required: true },
-  _20_30_pax: { type: Number, required: true },
-  _30_plus_pax: { type: Number, required: true },
+  rate: { type: Number, required: true },
 });
 
 const Packages = mongoose.model("packages", packagesSchema);
